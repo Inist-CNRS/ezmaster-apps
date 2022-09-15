10745dcb40f67f17e68aa1fdf6ead4e3e9eb34a4
Nicolas Thouvenin <nthouvenin@gmail.com>
2022-09-15 14:54:39 +0200

    add zip fetch

--- applications/lodex-crontab/docker-entrypoint.sh
+++ applications/lodex-crontab/docker-entrypoint.sh
@@ -2,6 +2,15 @@
 
 cd /app || exit 1;
 
+# configuration
+ZIP_URL=`node -e 'process.stdout.write(Array().concat(require("./config.json").files.zip).filter(Boolean).pop()||"")'`
+
+# récupération des fichiers
+if [ -n "${ZIP_URL}" ]
+then
+	/app/zipsyncdir "$ZIP_URL"
+fi
+
 # Restauration du owner
 find /app/public ! -user daemon -exec chown daemon:daemon {} \; &
 find /tmp ! -user daemon -exec chown daemon:daemon {} \; &
