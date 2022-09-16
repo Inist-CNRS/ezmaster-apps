b0140f646321daf34e2325a90d4f76171f654819
Nicolas Thouvenin <nthouvenin@gmail.com>
2022-09-16 11:12:38 +0200

    works like workers

--- applications/lodex-crontab/docker-entrypoint.sh
+++ applications/lodex-crontab/docker-entrypoint.sh
@@ -2,6 +2,18 @@
 
 cd /app || exit 1;
 
+# installation des packges additionnels
+PACKAGES_MEMORY="./packages.installed"
+PACKAGES=`node -e 'process.stdout.write(Array().concat(require("./config.json").packages).filter(Boolean).join(" ").trim())'`
+
+touch ${PACKAGES_MEMORY}
+PACKAGES_CHECK=`cat ${PACKAGES_MEMORY}`
+if [ -n "${PACKAGES}" ] && [ "${PACKAGES_CHECK}" != "${PACKAGES}" ]
+then
+	npm install --production  ${PACKAGES}
+	echo "${PACKAGES}" > ${PACKAGES_MEMORY}
+fi
+
 # configuration
 ZIP_URL=`node -e 'process.stdout.write(Array().concat(require("./config.json").files.zip).filter(Boolean).pop()||"")'`
 
