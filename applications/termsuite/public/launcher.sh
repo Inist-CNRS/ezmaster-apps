a5feee1e3846c4e11cf992568eff7a85f7085602
schneist <stephane.schneider@inist.fr>
2023-02-27 15:02:02 +0100

    goog

--- applications/termsuite/public/launcher.sh
+++ applications/termsuite/public/launcher.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
-
 debug() {
     echo "$@" 1>&2;
 }
@@ -10,27 +8,34 @@ exec_termsuite() {
 
     INPUT_CORPUS=$1
     
-    # Termsuite exec
+    # Termsuite runtime jar
     JAR=$(ls /opt/termsuite-core-*.jar)
 
+    # tempory dir inside the container for storage
     OUTPUT_HOME=/tmp/  # docker-ws version
 
     TOKEN=`basename $INPUT_CORPUS`_result
     OUTPUT_PATH=${OUTPUT_HOME}${TOKEN}
 
-    # name of the file result
-    F_RESULT=${TOKEN}.tsv
+    # file result
     mkdir -p ${OUTPUT_PATH}
-
+    F_RESULT=${TOKEN}.tsv
     F=${OUTPUT_PATH}/${F_RESULT}
-    # simple test
-    #echo -n "one result " >> $F # simul out of docker : fake result file just for test 
-    # termsuite en docker-ws
-    java -Xms1g -Xmx2g -cp ${JAR} fr.univnantes.termsuite.tools.TerminologyExtractorCLI  -c  ${INPUT_CORPUS} --tsv $F -l en -t /opt/treetagger
-    
-    #stockage endpoint 
+
+    #   1 - execution termsuite 
+    java -Xms16g -Xmx48g -cp ${JAR} fr.univnantes.termsuite.tools.TerminologyExtractorCLI  -c  ${INPUT_CORPUS} --tsv $F -l en -t /opt/treetagger &
+    # async mode
+    message="execution launching"
+    echo '{"value":"'${message}'", "code":0}'
+
+    wait # waits for all child background jobs to complete.
+
+    # termsuite hors docker-ws
+    #~/termsuite/termsuite-docker/bin/termsuite extract  -c  ${INPUT_CORPUS} -l en  --tsv  $F
+    # storage for the final result
     endpoint_storage=${WEBDAV_URL}
 
+    #   2 -  reg the result on the storage
     # check if a result exist 
     if [ -s $F ]
 
@@ -41,14 +46,14 @@ exec_termsuite() {
         curl -s -u "${WEBDAV_USER}:${WEBDAV_PAS}"  --proxy ""  -X PUT  --data-binary @- "${Res}"  -o /dev/null && \
         echo '{"value":"'${Res}'", "code":0}'
 
-        debug "END : result file available : ${F_RESULT}.gz to ${endpoint_storage}" && \
+        debug "sending result file ${F_RESULT}.gz to ${endpoint_storage}" && \
         debug "rm -rf ${INPUT_CORPUS}  ${OUTPUT_PATH}"  && \
         rm -rf ${INPUT_CORPUS}  ${OUTPUT_PATH} > /dev/null  # delete input and output file from tmp
         
-
     else
         debug "$0 error : code 1"
-        echo '{"value":"", "code":1}'
+        message="no data to delivery"
+        echo '{"value":"'${message}'", "code":1}'
     fi    
 
 }
@@ -56,6 +61,6 @@ exec_termsuite() {
 debug "$0 is waiting for data"
 
 while IFS= read -r line; do
-    debug "$0 : BEGIN : receive ${line}, processing in progress ..."
+    debug "$0 receive ${line}"
     exec_termsuite $1 
 done < <(cat -)
