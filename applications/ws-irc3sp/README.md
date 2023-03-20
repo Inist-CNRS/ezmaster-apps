79f2436a0141cd906d00c45b98e0e31ea61dcc05
François Parmentier <francois.parmentier@gmail.com>
2023-03-20 16:50:36 +0100

    feat(ws-irc3sp): Add IRC3
    First working version

---
+++ applications/ws-irc3sp/README.md
@@ -0,0 +1,15 @@
+# ws-irc3sp@0.0.0
+
+IRC3: Indexation par Recherche et Comparaison de Chaînes de Caractères
+
+Applied on **sp**ecies.
+
+## Test
+
+TODO: Change the sample
+
+```bash
+curl -X POST "http://localhost:31976/v1/irc3sp" \
+     -H  "accept: application/json" -H  "Content-Type: application/json" \
+     -d "[{\"id\":1,\"value\":\"UAR 76, Inist-CNRS, 2 rue Jean Zay 54500 Vandoeuvre-lès-Nancy\"},{\"id\":2,\"value\":\"Institut Charles Gerhardt, Université de Montpellier\"},{\"id\":3,\"value\":\"UMR 7272 - CNRS, 06100 Nice\"}]"
+```
---
+++ applications/ws-irc3sp/docker-entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# installation des packges additionnels
+PACKAGES_MEMORY="./packages.installed"
+PACKAGES=$(node -e 'process.stdout.write(Array().concat(require("./config.json").packages).filter(Boolean).join(" ").trim())')
+
+touch ${PACKAGES_MEMORY}
+PACKAGES_CHECK=$(cat ${PACKAGES_MEMORY})
+if [ -n "${PACKAGES}" ] && [ "${PACKAGES_CHECK}" != "${PACKAGES}" ]
+then
+	npm install --production  ${PACKAGES}
+	echo "${PACKAGES}" > ${PACKAGES_MEMORY}
+fi
+
+# configuration
+ZIP_URL=$(node -e 'process.stdout.write(Array().concat(require("./config.json").files.zip).filter(Boolean).pop()||"")')
+
+# récupération des fichiers
+if [ -n "${ZIP_URL}" ]
+then
+	/app/zipsyncdir "$ZIP_URL"
+fi
+
+# Restauration du owner
+find /app/public ! -user daemon -exec chown daemon:daemon {} \; &
+find /tmp ! -user daemon -exec chown daemon:daemon {} \; &
+
+# lancement des daemons
+su-exec daemon:daemon /app/public/v1/irc3_wrapper.sh ws
+npm run watcher &
+exec su-exec daemon:daemon npm start
