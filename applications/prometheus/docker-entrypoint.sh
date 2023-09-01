a13936fff97c48d931e4a421ad0a5c211eeff44a
Nicolas Thouvenin <nthouvenin@gmail.com>
2023-09-01 16:28:49 +0200

    fix: missing letter

--- applications/prometheus/docker-entrypoint.sh
+++ applications/prometheus/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 find /app ! -user daemon -exec chown daemon:daemon {} \; &
 find /tmp ! -user daemon -exec chown daemon:daemon {} \; &
 
-cat /app/prometheus.global.yml /app/data/config/*.yml /app/data/config/*.yml > /app/prometheus.yml 2>/dev/null
+cat /app/prometheus.global.yml /app/data/config/*.yml /app/data/config/*.yaml > /app/prometheus.yml 2>/dev/null
 /reload-config.sh &
 
 sudo -u daemon -g daemon /opt/prometheus/prometheus \
--- applications/prometheus/reload-config.sh
+++ applications/prometheus/reload-config.sh
@@ -3,6 +3,6 @@
 while true
 do
 		inotifywait -qr -e modify -e create -e delete -e move --exclude '/\.' /app/data/config;
-		cat /app/prometheus.global.yml /app/data/config/*.yml /app/data/config/*.yml > /app/prometheus.yml 2>/dev/null
+		cat /app/prometheus.global.yml /app/data/config/*.yml /app/data/config/*.yaml > /app/prometheus.yml 2>/dev/null
 		curl -s -X POST localhost:9090/-/reload
 done
