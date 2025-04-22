a97f28c8937f9b1df1bbbd1e8006aa3ff3d717c2
Nicolas Thouvenin <nthouvenin@gmail.com>
2025-04-22 17:32:35 +0200

    enable to set retention time at start up

--- applications/prometheus/docker-entrypoint.sh
+++ applications/prometheus/docker-entrypoint.sh
@@ -6,8 +6,10 @@ find /tmp ! -user daemon -exec chown daemon:daemon {} \; &
 cat /app/prometheus.global.yml /app/data/config/*.yml /app/data/config/*.yaml > /app/prometheus.yml 2>/dev/null
 /reload-config.sh &
 
+RTIME=`cat prometheus.yml | yq '.global.external_labels.ezmaster_retention_time'`
+
 sudo -u daemon -g daemon /opt/prometheus/prometheus \
 	--config.file=/app/prometheus.yml \
 	--web.enable-lifecycle \
 	--storage.tsdb.path=/app/data/prometheus \
-	--storage.tsdb.retention.time=90d
+	--storage.tsdb.retention.time=${RTIME}
