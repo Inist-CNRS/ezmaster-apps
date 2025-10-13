be31d601b5d67e9be2e4f8466685e38b72e8c319
Nicolas Thouvenin <nthouvenin@gmail.com>
2025-10-13 16:26:05 +0200

    upgrade version

--- applications/grafana/docker-entrypoint.sh
+++ applications/grafana/docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+mkdir -p /opt/grafana/data/plugins/
+
 find /app ! -user daemon -exec chown daemon:daemon {} \; &
 find /tmp ! -user daemon -exec chown daemon:daemon {} \; &
+find /opt/grafana/data ! -user daemon -exec chown daemon:daemon {} \; &
 
 sudo -u daemon -g daemon /opt/grafana/bin/grafana-server \
 	-homepath /opt/grafana \
