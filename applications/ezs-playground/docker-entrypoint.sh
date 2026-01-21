85451eb04e572a22046052fe5330beb4fe9764ec
Nicolas Thouvenin <nthouvenin@gmail.com>
2026-01-21 18:12:38 +0100

    drop dynamic npm install

--- applications/ezs-playground/docker-entrypoint.sh
+++ applications/ezs-playground/docker-entrypoint.sh
@@ -2,18 +2,6 @@
 
 cd /app || exit 1;
 
-# installation des packges additionnels
-PACKAGES_MEMORY="./packages.installed"
-PACKAGES=`node -e 'process.stdout.write(Array().concat(require("./config.json").packages).filter(Boolean).join(" ").trim())'`
-
-touch ${PACKAGES_MEMORY}
-PACKAGES_CHECK=`cat ${PACKAGES_MEMORY}`
-if [ -n "${PACKAGES}" ] && [ "${PACKAGES_CHECK}" != "${PACKAGES}" ]
-then
-	npm install --production  ${PACKAGES}
-	echo "${PACKAGES}" > ${PACKAGES_MEMORY}
-fi
-
 # generate .env
 node ./generate-dotenv.js
 
