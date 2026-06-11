437d07c4d32561758d8d00292210ebfae9deb8ba
Nicolas Thouvenin <nthouvenin@gmail.com>
2026-06-11 16:51:33 +0200

    mongodb 8.3.3

--- applications/mongo/docker-entrypoint.overload.sh
+++ applications/mongo/docker-entrypoint.overload.sh
@@ -2,13 +2,6 @@
 
 chown -R mongod:mongod /ezdata/db
 
-# initialize data flags used by inherited docker-entrypoint.sh
-#test ! -f /ezdata/db/WiredTiger && rm -f /data/db/WiredTiger
-#test   -f /ezdata/db/WiredTiger && ln -s /ezdata/db/WiredTiger /data/db/
-#test ! -f /ezdata/db/docker-initdb.log && rm -f /data/db/docker-initdb.log
-#test   -f /ezdata/db/docker-initdb.log && ln -s /ezdata/db/docker-initdb.log /data/db/
-su-exec mongod:mongod touch /ezdata/db/TEST
-
 # basic http server for displaing a basic informative html page for ezmaster
 cd /www && su-exec mongod:mongod python3 -m http.server 3000 &
 
